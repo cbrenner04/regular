53903d6d8c5806a9da7139cfcfe5eb12a2ec6302
Alan <a-ashbeck@northwestern.edu>
2017-03-07 18:32:03 -0600

    Add basic stormpath setup

--- server.js
+++ server.js
@@ -2,10 +2,25 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var path = require('path');
 
+// For Storm Path
+var ReactStormpath = require('react-stormpath').default;
+var Router = require('react-stormpath').Router;
+var AuthenticatedRoute = require('react-stormpath').AuthenticatedRoute;
+var LoginLink = require('react-stormpath').LoginLink;
+
 var PORT = 3000;
 
 var app = express();
 var port = process.env.PORT || PORT;
+
+
+ReactStormpath.init({
+  endpoints: {
+    baseUri: '***'
+    devUri: 'http://localhost:3000'
+    productionUri: 'https://cjb-final-project.herokuapp.com/'
+  }
+});
 
 // Serve static content for the app from 'public' directory
 app.use(express.static(path.join(__dirname, '/public')));
