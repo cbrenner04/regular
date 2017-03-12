811f543f3baa0e92df56477aa7de4bfc793388f8
Alan <a-ashbeck@northwestern.edu>
2017-03-12 14:22:28 -0500

    Update components

---
+++ app/components/UnAuthLanding.js
@@ -0,0 +1,10 @@
+import {Link} from 'react-router';
+import {LoginLink} from 'react-stormpath';
+import React from 'react';
+
+const UnAuthLanding = () =>
+    <div className="container">
+        <p>UnAuthLanding</p>
+    </div>
+
+export default UnAuthLanding;
--- app/config/routes.js
+++ app/config/routes.js
@@ -1,21 +1,23 @@
 import {AuthenticatedRoute, HomeRoute, LoginRoute, Router}
   from 'react-stormpath';
 import {IndexRoute, Route, browserHistory} from 'react-router';
-import IndexPage from '../components/IndexPage';
+import UnAuthLanding from '../components/UnAuthLanding';
+import AuthLanding from '../components/AuthLanding';
 import LoginPage from '../components/LoginPage';
 import Main from '../components/main';
-import ProfilePage from '../components/ProfilePage';
+import Profile from '../components/Profile';
 import React from 'react';
 import RegistrationPage from '../components/RegistrationPage';
 
 const Routes = () =>
     <Router history={ browserHistory }>
         <HomeRoute path="/" component={ Main }>
-            <IndexRoute component={ IndexPage } />
+            <IndexRoute component={ UnAuthLanding } />
             <LoginRoute path="/login" component={ LoginPage } />
             <Route path="/register" component={ RegistrationPage } />
             <AuthenticatedRoute>
-                <HomeRoute path="/profile" component={ ProfilePage } />
+                <HomeRoute path="/home" component={ AuthLanding } />
+                <Route path="/profile" component={ Profile } />
             </AuthenticatedRoute>
           </HomeRoute>
     </Router>
