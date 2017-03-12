b8c458ba9233b7fa926743aa2c80db4f1bb89bf5
maxvanbel <maxvanbel@gmail.com>
2017-03-12 16:13:49 -0500

    add UnAthLanding content

--- app/components/UnAuthLanding.js
+++ app/components/UnAuthLanding.js
@@ -1,10 +1,20 @@
 import {Link} from 'react-router';
-import {LoginLink} from 'react-stormpath';
+import {LoginLink, NotAuthenticated} from 'react-stormpath';
 import React from 'react';
 
 const UnAuthLanding = () =>
     <div className="container">
-        <p>UnAuthLanding</p>
+      <div className="jumbotron text-center" id="jumbo">
+        <div className="main-text">
+          <h1>REGULAR</h1>
+            <h3>Rate and find any bathroom in the city</h3>
+            <h4>Made the Editor's Pick on the Apple App store</h4>
+            <NotAuthenticated>
+              <Link className="btn btn-primary" to="/register">Register!</Link>
+            </NotAuthenticated>
+            <a className="btn btn-success">About Us</a>
+        </div>
+      </div>
     </div>
 
 export default UnAuthLanding;
