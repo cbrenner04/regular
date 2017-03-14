b6164ae833ab2976ab120588f72e49bca28e9f60
maxvanbel <maxvanbel@gmail.com>
2017-03-14 18:32:40 -0500

    add models

---
+++ models/establishment.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+mongoose.models = {};
+mongoose.modelSchemas = {};
+var establishmentSchema = new Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    }
+});
+var Establishment = mongoose.model('Establishment', establishmentSchema);
+module.exports = Establishment;
