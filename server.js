765cb42d9014d79e20f8f5a7594d15f9f816b531
maxvanbel <maxvanbel@gmail.com>
2017-03-14 17:58:23 -0500

    add mongoose to server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var express = require('express');
 var path = require('path');
@@ -9,6 +10,20 @@ var LOCAL_PORT = 3000;
 
 var app = express();
 var port = process.env.PORT || LOCAL_PORT;
+var db;
+
+
+mongoose.connect();
+
+db = mongoose.connection;
+
+mongoose.Promise = Promise;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('MONGOOSE is working');
+    // we're connected!
+});
 
 app.set('trust proxy');
 
@@ -26,7 +41,6 @@ app.on('stormpath.ready', function () {
         if (err) {
             return console.error(err);
         }
-
         /* eslint no-console: "off", prefer-template: "off" */
         return console.log('listening on port ' + port);
     });
