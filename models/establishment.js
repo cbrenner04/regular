377e727c4a51fd3f60f5ad3b5a21469681505548
maxvanbel <maxvanbel@gmail.com>
2017-03-14 19:30:30 -0500

    finish basics for models

--- models/establishment.js
+++ models/establishment.js
@@ -5,7 +5,8 @@ mongoose.modelSchemas = {};
 var establishmentSchema = new Schema({
     id: {
         type: Number,
-        required: true
+        required: true,
+        unique: true
     },
     name: {
         type: String,
