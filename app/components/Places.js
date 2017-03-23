8fe778261002052e4b88893cc7c0e808abd23013
Alan <a-ashbeck@northwestern.edu>
2017-03-23 10:05:28 -0500

    Fix places code

--- app/components/Places.js
+++ app/components/Places.js
@@ -3,16 +3,16 @@ import React from 'react'
 const Places = ({venues}) =>
     <div>
         <h1 className="text-center">Possible Venues with Bathrooms</h1>
-            {
-                venues.map((venue, index) =>
-                        <a
-                            href={`/#/establishments/${venue.id}`}
-                            className="btn btn-default btn-lg btn-block"
-                        >
-                            {`${index + 1}. ${venue.name}`}
-                        </a>
-                )
-            }
+        {
+            venues.map((venue, index) =>
+                <a
+                    href={`/#/establishments/${venue.id}`}
+                    className="btn btn-default btn-lg btn-block"
+                >
+                    {`${index + 1}. ${venue.name}`}
+                </a>
+            )
+        }
     </div>
 
 export default Places
