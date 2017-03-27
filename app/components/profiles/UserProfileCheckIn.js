b4be47ba5ca1eaccddba163998a9d35c844d70c8
maxvanbel <maxvanbel@gmail.com>
2017-03-27 18:53:52 -0500

    fix timestamp format

--- app/components/profiles/UserProfileCheckIn.js
+++ app/components/profiles/UserProfileCheckIn.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import moment from 'moment';
 const UserProfileCheckIn = ({establishments}) =>
     <div className="row">
         {
             establishments.map((establishment) =>
                 <div className="well well-sm" key={establishment._id}>
                     <p><b>Name:</b> {establishment.establishment.name}</p>
-                    <p><b>Checked In:</b> {establishment.createdAt}</p>
+                    <p><b>Checked In:</b> {moment(establishment.createdAt).format('LLL')}</p>
                 </div>
             )
         }
