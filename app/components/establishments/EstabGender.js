7868cbab088a6096e05506817c5063109307553c
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-03-27 19:26:14 -0500

    Update form styles to show

--- app/components/establishments/EstabGender.js
+++ app/components/establishments/EstabGender.js
@@ -1,43 +1,88 @@
 import React, {Component} from 'react';
 
+const FEMALE_SELECTOR = 'female';
+const MALE_SELECTOR = 'male';
+const GENDER_NEUTRAL_SELECTOR = 'gender neutral';
+const FAMILY_SELECTOR = 'family';
+
 export default class EstabGender extends Component {
+    constructor() {
+        super();
+        this.state = {
+            femaleButtonStyle: 'default',
+            maleButtonStyle: 'default',
+            genderNeutralButtonStyle: 'default',
+            familyButtonStyle: 'default'
+        }
+    }
+
     onSelection(gender) {
+        if (gender === FEMALE_SELECTOR) {
+            this.setState({
+                femaleButtonStyle: 'warning',
+                maleButtonStyle: 'default',
+                genderNeutralButtonStyle: 'default',
+                familyButtonStyle: 'default'
+            })
+        } else if (gender === MALE_SELECTOR) {
+            this.setState({
+                femaleButtonStyle: 'default',
+                maleButtonStyle: 'warning',
+                genderNeutralButtonStyle: 'default',
+                familyButtonStyle: 'default'
+            })
+        } else if (gender === GENDER_NEUTRAL_SELECTOR) {
+            this.setState({
+                femaleButtonStyle: 'default',
+                maleButtonStyle: 'default',
+                genderNeutralButtonStyle: 'warning',
+                familyButtonStyle: 'default'
+            })
+        } else if (gender === FAMILY_SELECTOR) {
+            this.setState({
+                femaleButtonStyle: 'default',
+                maleButtonStyle: 'default',
+                genderNeutralButtonStyle: 'default',
+                familyButtonStyle: 'warning'
+            })
+        }
         this.props.onGenderResponse(gender);
     }
+
     render() {
         return (
             <div className="row">
                 <div className="gender-main">
                     <h2>What bathroom are you using?</h2>
                     <div className="col-xs-12">
-                        <button className="btn btn-default"
+                        <button className={`btn btn-${this.state.femaleButtonStyle}`}
                                 onClick={
                                     () => {
-                                        this.onSelection('female')
+                                        this.onSelection(FEMALE_SELECTOR)
                                     }
                                 }>
                             Female
                         </button>
-                        <button className="btn btn-default"
+                        <button className={`btn btn-${this.state.maleButtonStyle}`}
                                 onClick={
                                     () => {
-                                        this.onSelection('male')
+                                        this.onSelection(MALE_SELECTOR)
                                     }
                                 }>
                             Male
                         </button>
-                        <button className="btn btn-default"
+                        <button className={`btn btn-${this.state.genderNeutralButtonStyle}`}
                                 onClick={
                                     () => {
-                                        this.onSelection('gender neutral')
+                                        this.onSelection(GENDER_NEUTRAL_SELECTOR)
                                     }
                                 }>
                             Gender Neutral
                         </button>
-                        <button className="btn btn-default"
+                        <button className={`btn btn-${this.state.familyButtonStyle}`}
                                 onClick={
                                     () => {
-                                        this.onSelection('family')
+                                        this.onSelection(FAMILY_SELECTOR)
                                     }
                                 }>
                             Family
