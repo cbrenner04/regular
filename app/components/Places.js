ca011346c425b7133c3ee879eb90f7c69b6568bd
Alan <a-ashbeck@northwestern.edu>
2017-03-29 09:41:52 -0500

    Save changes for later

--- app/components/Places.js
+++ app/components/Places.js
@@ -4,16 +4,35 @@ import {Button, Modal} from 'react-bootstrap';
 export default class Places extends Component {
     constructor() {
         super();
-        this.state = { showModal: true }
+        this.state = { places: [] }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            places: nextProps.venues.map((venue) => {
+                return ({
+                    name: venue.name
+                })
+            })
+        })
+    }
+
+    open(targetPlace) {
+        this.setState({
+            places: this.state.places.map((place) => {
+                const modalStatus = place.name === targetPlace.name
+                return Object.assign({}, place, { showInfo: modalStatus })
+            })
+        })
     }
 
     close() {
         this.setState({ showModal: false });
     }
 
-    open() {
-        this.setState({ showModal: true });
-    }
+    // open() {
+    //     this.setState({ showModal: true });
+    // }
 
     render() {
         const OFFSET = 1;
@@ -22,31 +41,47 @@ export default class Places extends Component {
             <div>
                 <h1 className="text-center places">Possible Venues with Bathrooms</h1>
                 {
-                    this.props.venues.map((venue, index) =>
-                        <div>
-                            <Button
-                                bsStyle="primary"
-                                bsSize="large"
-                                key={venue.id}
-                                onClick={this.open}
-                            >
-                                 {`${index + OFFSET}. ${venue.name}`}
-                            </Button>
-                        </div>
-                    )
+                    this.state.places.map((place, index) => {
+                        const modal = {
+                            name: place.name,
+                            showModal: place.showInfo
+                        }
+
+                        return (
+                            <div>
+                                <Button
+                                    bsStyle="primary"
+                                    bsSize="large"
+                                    key={place.id}
+                                    onClick={
+                                        () => {this.open(modal)}
+                                }>
+                                     {`${index + OFFSET}. ${place.name}`}
+                                </Button>
+
+                                <Modal show={this.state.showModal} onHide={
+                                        () => {this.close()}
+                                }>
+                                    <Modal.Header closeButton>
+                                        <Modal.Title>{`${index + OFFSET}. ${place.name}`}</Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body>
+                                        {`${index + OFFSET}. ${place.name}`}
+                                    </Modal.Body>
+                                    <Modal.Footer>
+                                        <Button onClick={
+                                            () => {this.close()}
+                                        }>
+                                            Close
+                                        </Button>
+                                    </Modal.Footer>
+                                </Modal>
+                            </div>
+                        )
+                    })
                 }
 
-                <Modal show={this.state.showModal} onHide={this.close}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>TEST</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        test test test
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button onClick={this.close}>Close</Button>
-                    </Modal.Footer>
-                </Modal>
+                
             </div>
         )
     }
