43459e956397bd95efb9d364b7916a8f314edb3d
Alan <a-ashbeck@northwestern.edu>
2017-03-28 20:32:48 -0500

    Continue adding modal

--- app/components/Places.js
+++ app/components/Places.js
@@ -1,19 +1,53 @@
-import React from 'react'
+import React, {Component} from 'react';
+import {Button, Modal} from 'react-bootstrap';
 
-const OFFSET = 1;
+export default class Places extends Component {
+    constructor() {
+        super();
+        this.state = { showModal: false }
+    }
 
-const Places = ({venues}) =>
-    <div>
-        <h1 className="text-center places">Possible Venues with Bathrooms</h1>
-        {
-            venues.map((venue, index) =>
-                <a href={`/#/establishments/${venue.id}`}
-                   className="btn btn-default btn-lg btn-block places-well"
-                   key={venue.id}>
-                    {`${index + OFFSET}. ${venue.name}`}
-                </a>
-            )
-        }
-    </div>
+    close() {
+        this.setState({ showModal: false });
+    }
 
-export default Places
+    open() {
+        this.setState({ showModal: true });
+    }
+
+    render() {
+        const OFFSET = 1;
+
+        return (
+            <div>
+                <h1 className="text-center places">Possible Venues with Bathrooms</h1>
+                {
+                    this.props.venues.map((venue, index) =>
+                        <div>
+                            <Button
+                                bsStyle="primary"
+                                bsSize="large"
+                                key={venue.id}
+                                onClick={this.open}
+                            >
+                                 {`${index + OFFSET}. ${venue.name}`}
+                            </Button>
+                        </div>
+                    )
+                }
+
+                <Modal show={this.state.showModal} onHide={this.close}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>TEST</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        test test test
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={this.close}>Close</Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        )
+    }
+}
